fb94c8f262708e915e9730cb02cbd9dd9a2dd56b
MaThanhVan <119687191+MaThanhVan@users.noreply.github.com>
2023-06-14 22:44:31 +0700

    update file

--- B9_Exercise_Calculate_Strings/Exercise_Basic_Calculation/Source/Basic_Calculation.c
+++ B9_Exercise_Calculate_Strings/Exercise_Basic_Calculation/Source/Basic_Calculation.c
@@ -14,13 +14,14 @@
 * Description: This function use for caculate basic calculate in a string
 * Input:
 *   string of characters name input of type char
-    string of characters name ouput of type struct
+*    string of characters name ouput of type struct
 * Output:
 *   return: none
 */
 void String(char input[], typeMaths ouput[])
 {
     static uint16_t number = 0;
+    uint8_t i = 0;
     while ( *input != '\0')
     {
         
@@ -31,10 +32,28 @@ void String(char input[], typeMaths ouput[])
             input++;
             continue;       // den day se dung cac lenh phis duoi, quay lai doc thang tiep theo
         }
-        else{
-            printf("number: %d\n", number)
+        else if(*input == '+' || *input == '-' || *input == '*' || *input == '/')
+        {
+            ouput[i].key = OPERATOR;
+            ouput[i].value.opera = *input;
+            input++;
+            i++;
+            continue;
         }
+        else{
+            if(number != 0){
+
+                ouput[i].key = NUMBER;
+                ouput[i].value.number = number;
+                i++;
+                number = 0;
+            }
+        }
+        input++;
+        
     }
+    ouput[i].key = NUMBER;
+    ouput[i].value.number = number;
     
 }
 
--- B9_Exercise_Calculate_Strings/Exercise_Basic_Calculation/Source/main.c
+++ B9_Exercise_Calculate_Strings/Exercise_Basic_Calculation/Source/main.c
@@ -1,7 +1,11 @@
-#include "Basic_Caculation.c"
+/*
+* File Name: main.c
+* Author: MA THANH VAN
+* Date: 13/06/2023
+* Description: Using a basic caculate in a string to store and manage a list of data.
+*/
 
-
-
+#include "Basic_Calculation.c"
 
  int main(int argc, char const *argv[])
 {
